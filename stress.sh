9a20876d63814f55acf054399975ecf125950036
EmmaBeni <lecbeni@mail.austral.edu.ar>
2026-03-14 12:16:26 -0300

    prueba de bash

---
+++ stress.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+for i in {1..100}; do
+{
+    start=$(date +%s%N)
+    curl -s -o /dev/null "http://localhost:3000/pi/1000000"
+    end=$(date +%s%N)
+
+    elapsed_ns=$((end - start))
+    elapsed_ms=$((elapsed_ns / 1000000))
+
+    echo "Curl $i terminó en ${elapsed_ms} ms"
+} &
+done
+
+wait
+echo "Listo: los 16 curls terminaron"
